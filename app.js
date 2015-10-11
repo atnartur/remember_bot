7574a4344d3d2524440a1e44d134c0bcd86e4490
atnartur <atnartur2012@gmail.com>
2015-10-11 17:08:36 +0300

    + команда /echo

--- app.js
+++ app.js
@@ -4,21 +4,24 @@
  */
 
 var TelegramBot = require('node-telegram-bot-api');
-var config = require('config.json');
-var token = 'YOUR_TELEGRAM_BOT_TOKEN';
+var config = require('./config.json');
+
 // Setup polling way
-var bot = new TelegramBot(token, {polling: true});
+var bot = new TelegramBot(config.api.telegram.token, {polling: true});
+
+console.log('app started');
 
 bot.onText(/\/echo (.+)/, function (msg, match) {
     var fromId = msg.from.id;
     var resp = match[1];
     bot.sendMessage(fromId, resp);
+    console.log(fromId);
 });
 
 // Any kind of message
-bot.on('message', function (msg) {
-    var chatId = msg.chat.id;
-    // photo can be: a file path, a stream or a Telegram file_id
-    var photo = 'cats.png';
-    bot.sendPhoto(chatId, photo, {caption: 'Lovely kittens'});
-});
+//bot.on('message', function (msg) {
+//    var chatId = msg.chat.id;
+//    // photo can be: a file path, a stream or a Telegram file_id
+//    var photo = 'cats.png';
+//    bot.sendPhoto(chatId, photo, {caption: 'Lovely kittens'});
+//});
